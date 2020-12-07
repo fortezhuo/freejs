2631bcae201b8cf460fe8630446d542d336f315c
Forte <hedikasmanto@gmail.com>
2020-12-07 15:08:29 +0700

    Checkpoint

--- packages/core/component/Routes/DrawerScreen.tsx
+++ packages/core/component/Routes/DrawerScreen.tsx
@@ -1,10 +1,12 @@
 import React from "react"
-import { IconButton, Sidebar, Header } from ".."
+import { IconButton, Sidebar, Header, Gradient } from ".."
 import { DrawerActions } from "@react-navigation/native"
 import { StyleSheet } from "react-native"
 import { createDrawerNavigator } from "@react-navigation/drawer"
 import { tw } from "@free/tailwind"
 import { random } from "../../util"
+import { theme } from "../../config/theme"
+const colors = [theme.primary_1_bg, theme.primary_2_bg]
 
 const Drawer = createDrawerNavigator()
 
@@ -15,49 +17,53 @@ export const DrawerScreen: React.FC<any> = ({
   navigation,
 }) => {
   return (
-    <Drawer.Navigator
-      sceneContainerStyle={{
-        backgroundColor: "transparent",
-      }}
-      screenOptions={{
-        headerStyle: {
+    <Gradient style={{ flex: 1 }} colors={colors}>
+      <Drawer.Navigator
+        sceneContainerStyle={{
           backgroundColor: "transparent",
-          borderBottomColor: "transparent",
-        },
-        headerLeft: () => {
-          return isMobile ? (
-            <IconButton
-              name={"menu"}
-              style={s.headerLeft}
-              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+        }}
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "transparent",
+            borderBottomColor: "transparent",
+          },
+          headerLeft: () => {
+            return isMobile ? (
+              <IconButton
+                name={"menu"}
+                style={s.headerLeft}
+                onPress={() =>
+                  navigation.dispatch(DrawerActions.toggleDrawer())
+                }
+              />
+            ) : undefined
+          },
+          headerRight: () => <Header.MenuUser />,
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: "normal",
+          },
+          headerShown: true,
+        }}
+        drawerType={isMobile ? "slide" : "permanent"}
+        drawerContent={(props) => {
+          return <Sidebar {...props} />
+        }}
+      >
+        <Drawer.Screen name="Index" component={screens.PageHome} />
+        {routes.view.map((route: any) => {
+          return (
+            <Drawer.Screen
+              key={"drawer_" + random()}
+              options={{ title: route.title }}
+              name={route.name}
+              component={screens[route.component]}
             />
-          ) : undefined
-        },
-        headerRight: () => <Header.MenuUser />,
-        headerTintColor: "white",
-        headerTitleStyle: {
-          fontSize: 24,
-          fontWeight: "normal",
-        },
-        headerShown: true,
-      }}
-      drawerType={isMobile ? "slide" : "permanent"}
-      drawerContent={(props) => {
-        return <Sidebar {...props} />
-      }}
-    >
-      <Drawer.Screen name="Index" component={screens.PageHome} />
-      {routes.view.map((route: any) => {
-        return (
-          <Drawer.Screen
-            key={"drawer_" + random()}
-            options={{ title: route.title }}
-            name={route.name}
-            component={screens[route.component]}
-          />
-        )
-      })}
-    </Drawer.Navigator>
+          )
+        })}
+      </Drawer.Navigator>
+    </Gradient>
   )
 }
 
