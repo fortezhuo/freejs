f4159b85ee787988a60cbee13788c6fb4198de90
Forte <hedikasmanto@gmail.com>
2021-01-05 17:40:00 +0700

    Enhance Security

--- packages/mobile/screens/index.tsx
+++ packages/mobile/screens/index.tsx
@@ -1,5 +1,6 @@
 export { default as PageHome } from "@free/core/screen/PageHome"
 export { default as Page500 } from "@free/core/screen/Page500"
+export { default as Page404 } from "@free/core/screen/Page404"
 export { default as PageLogin } from "@free/core/screen/PageLogin"
 export { default as ViewGrid } from "@free/core/screen/ViewGrid"
 export { default as SettingUser } from "@free/core/screen/SettingUser"
