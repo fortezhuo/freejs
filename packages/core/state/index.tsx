0c93d735d4deee0c654bea298458df0c677b184e
Forte <hedikasmanto@gmail.com>
2021-01-12 19:36:22 +0700

    Checkpoint

--- packages/core/state/index.tsx
+++ packages/core/state/index.tsx
@@ -11,51 +11,15 @@ const acl = new RBAC()
 const useHook = () => {
   const refAlert = React.useRef(null)
   const app = useDefaultState({})
-  const [access, setAccess] = React.useState(0)
-  const refresh = React.useCallback(() => setAccess((access) => access++), [])
-
-  React.useEffect(() => {
-    if (!app.data.auth?.username) {
-      ;(async function () {
-        try {
-          app.setState({
-            isLoading: true,
-          })
-          const res = await req.GET("/api/auth")
-
-          app.setData({ auth: res.data.result })
-        } catch (e) {
-          console.log("AUTH FAILED", e)
-        } finally {
-          app.setState({ isLoading: false })
-        }
-      })()
-    } else {
-      const { access, roles = [] } = app.data.auth
-      acl.load(access)
-      acl.register(roles, {})
-      refresh()
-    }
-  }, [app.data.auth?.username])
-
-  const can = React.useCallback(
-    (action: string, target: string) => {
-      const { granted }: any = acl.can(action, target)
-      return granted
-    },
-    [
-      app.data?.auth?.username,
-      app.data?.auth?.access,
-      app.data?.auth?.roles,
-      access,
-    ]
-  )
 
   const login = React.useCallback(async (data) => {
     try {
       app.setError({})
       app.setState({ isUpdating: true })
       const res = await req.POST("/api/auth", { ...data })
+      const { access, roles = [] } = res.data.result
+      acl.load(access)
+      acl.register(roles, {})
       app.setData({ auth: res.data.result })
     } catch (err) {
       app.setError(err)
@@ -68,6 +32,35 @@ const useHook = () => {
     await req.GET("/api/auth/logout")
     app.setData({ auth: undefined })
   }, [app.data?.auth?.username])
+
+  const can = React.useCallback(
+    (action: string, target: string) => {
+      const { granted }: any = acl.can(action, target)
+      return granted
+    },
+    [app.data?.auth?.username]
+  )
+
+  React.useEffect(() => {
+    if (!app.data.auth?.username) {
+      ;(async function () {
+        try {
+          app.setState({
+            isLoading: true,
+          })
+          const res = await req.GET("/api/auth")
+          const { access, roles = [] } = res.data.result
+          acl.load(access)
+          acl.register(roles, {})
+          app.setData({ auth: res.data.result })
+        } catch (e) {
+          console.log("AUTH FAILED", e)
+        } finally {
+          app.setState({ isLoading: false })
+        }
+      })()
+    }
+  }, [app.data.auth?.username])
 
   return { ...app, can, login, logout, refAlert }
 }
